209b32815fb361e0bd61edeb11df398078475e52
jakob <jakobzhao@gmail.com>
2017-09-24 23:53:53 -0700

    reading list highlighted for week 01

--- labs/lab03/js/main.js
+++ labs/lab03/js/main.js
@@ -6,75 +6,83 @@ var mymap = L.map('map', {center: [44.13, -119.93], zoom: 7});
 
 L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(mymap);
 
-// Create Custom Icons Here
-var TowerIcon = L.Icon.extend({
-    options:{
-        shadowUrl: 'img/ct_sd.png',
-        iconSize: [18,18],
-        shadowSize: [25,18],
-        iconAnchor: [16, 16],
-        shadowAnchor: [16, 16],
-        popupAnchor: [-8, -18]
-    }
-});
 
-var tower1Icon = new TowerIcon({iconUrl: 'img/ct1.png'});
-var tower2Icon = new TowerIcon({iconUrl: 'img/ct2.png'});
-var tower3Icon = new TowerIcon({iconUrl: 'img/ct3.png'});
-var tower4Icon = new TowerIcon({iconUrl: 'img/ct4.png'});
-var tower5Icon = new TowerIcon({iconUrl: 'img/ct5.png'});
-var tower6Icon = new TowerIcon({iconUrl: 'img/ct6.png'});
-var tower7Icon = new TowerIcon({iconUrl: 'img/ct7.png'});
-var tower8Icon = new TowerIcon({iconUrl: 'img/ct8.png'});
-var tower9Icon = new TowerIcon({iconUrl: 'img/ct9.png'});
+//https://github.com/gka/chroma.js/blob/master/src/colors/colorbrewer.coffee
+var colors = chroma.scale('Accent').mode('lch').colors(9);
 
-var cellTowers;
+for (i = 0; i < 9; i++) {
+    $('head').append($("<style> .marker-color-" + (i + 1).toString() + " { color: " + colors[i] + "; font-size: 15px; text-shadow: 0 0 3px #ffffff;} </style>"));
+}
+
 
 // Get GeoJSON and put on it on the map when it loads
-cellTowers = L.geoJson.ajax("assets/cell_towers.geojson", {
+L.geoJson.ajax("assets/cell_towers.geojson", {
     onEachFeature: function (feature, layer) {
         layer.bindPopup(feature.properties.company);
     },
     pointToLayer: function (feature, latlng) {
-        var marker = null;
-        if (feature.properties.company === "New Cingular"){
-            marker = L.marker(latlng,{icon: tower1Icon});
-        } else if (feature.properties.company === "Cello"){
-            marker = L.marker(latlng,{icon: tower2Icon});
-        } else if (feature.properties.company === "Hood River Cellular"){
-            marker = L.marker(latlng,{icon: tower3Icon});
-        } else if (feature.properties.company === "Medford Cellular"){
-            marker = L.marker(latlng,{icon: tower4Icon});
-        } else if (feature.properties.company === "Verizon"){
-            marker = L.marker(latlng,{icon: tower5Icon});
-        } else if (feature.properties.company === "Oregon RSA"){
-            marker = L.marker(latlng,{icon: tower6Icon});
-        } else if (feature.properties.company === "RSS Minnesota"){
-            marker = L.marker(latlng,{icon: tower7Icon});
-        } else if (feature.properties.company === "Salem Cellular"){
-            marker = L.marker(latlng,{icon: tower8Icon});
-        } else {
-            marker = L.marker(latlng,{icon: tower9Icon});
+        switch (feature.properties.company) {
+            case "New Cingular":
+                id = 0;
+                break;
+            case "Cellco":
+                id = 1;
+                break;
+            case "RCC Minnesota":
+                id = 2;
+                break;
+            case "Verizon":
+                id = 3;
+                break;
+            case "US Cellular":
+                id = 4;
+                break;
+            case "Hood River Cellular":
+                id = 5;
+                break;
+            case "Medford Cellular":
+                id = 6;
+                break;
+            case "Oregon RSA":
+                id = 7;
+                break;
+            default:
+                id = 8;// "Salem Cellular"
         }
-        return marker;
+        return L.marker(latlng, {icon: L.divIcon({className: 'fa fa-signal marker-color-' + (id + 1).toString() })});
     }
 }).addTo(mymap);
 
-
+//https://github.com/gka/chroma.js/blob/master/src/colors/colorbrewer.coffee
 // Set function for color ramp
-function setColor(density){
-    return density > 18 ? '#b30000' :
-           density > 13 ? '#e34a33' :
-           density > 10 ? '#fc8d59' :
-           density >  5 ? '#fdcc8a' :
-                          '#fef0d9';
+colors = chroma.scale('OrRd').mode('hsl').colors(5); //colors = chroma.scale('OrRd').colors(5);
+function setColor(density) {
+    flag = 0;
+    switch (true) {
+        case density > 18:
+            flag = 4;
+            break;
+        case density > 13 && density <= 18:
+            flag = 3;
+            break;
+        case density > 10 && density <= 13:
+            flag = 2;
+            break;
+        case density > 5 &&  density <= 10:
+            flag = 1;
+            break;
+        case density <= 5:
+            flag = 0;
+    }
+    return colors[flag];
 }
+
 
 // Set style function that sets fill color property equal to cell tower density
 function style(feature) {
     return {
         fillColor: setColor(feature.properties.CT_CNT),
-        fillOpacity: 0.2,
+        fillOpacity: 0.4,
         weight: 2,
         opacity: 1,
         color: '#ffffff',
@@ -82,13 +90,11 @@ function style(feature) {
     };
 }
 
-// Null variable that will hold counties layer
-var countiesLayer;
-
-// Add Counties Polygons
-countiesLayer = L.geoJson.ajax("assets/counties.geojson", {
+// Add Neighborhood Polygons
+L.geoJson.ajax("assets/counties.geojson", {
     style: style
 }).addTo(mymap);
+
 
 // Create Leaflet Control Object for Legend
 var legend = L.control({position: 'topright'});
@@ -99,21 +105,21 @@ legend.onAdd = function () {
     // Create Div Element and Populate it with HTML
     var div = L.DomUtil.create('div', 'legend');
     div.innerHTML += '<b># Cell Tower</b><br />';
-    div.innerHTML += '<i style="background: #b30000; opacity: 0.5"></i><p>19+</p>';
-    div.innerHTML += '<i style="background: #e34a33; opacity: 0.5"></i><p>14-18</p>';
-    div.innerHTML += '<i style="background: #fc8d59; opacity: 0.5"></i><p>11-13</p>';
-    div.innerHTML += '<i style="background: #fdcc8a; opacity: 0.5"></i><p> 6-10</p>';
-    div.innerHTML += '<i style="background: #fef0d9; opacity: 0.5"></i><p> 0- 5</p>';
+    div.innerHTML += '<i style="background: ' + colors[4] + '; opacity: 0.5"></i><p>19+</p>';
+    div.innerHTML += '<i style="background: ' + colors[3] + '; opacity: 0.5"></i><p>14-18</p>';
+    div.innerHTML += '<i style="background: ' + colors[2] + '; opacity: 0.5"></i><p>11-13</p>';
+    div.innerHTML += '<i style="background: ' + colors[1] + '; opacity: 0.5"></i><p> 6-10</p>';
+    div.innerHTML += '<i style="background: ' + colors[0] + '; opacity: 0.5"></i><p> 0- 5</p>';
     div.innerHTML += '<hr><b>Company<b><br />';
-    div.innerHTML += '<img src="img/ct1.png"><p> New Cingular</p>';
-    div.innerHTML += '<img src="img/ct2.png"><p> Cello</p>';
-    div.innerHTML += '<img src="img/ct3.png"><p> Hood River Cellular</p>';
-    div.innerHTML += '<img src="img/ct4.png"><p> Medford Cellular</p>';
-    div.innerHTML += '<img src="img/ct5.png"><p> Verizon</p>';
-    div.innerHTML += '<img src="img/ct6.png"><p> Oregon RSA</p>';
-    div.innerHTML += '<img src="img/ct7.png"><p> RSS MInnesota</p>';
-    div.innerHTML += '<img src="img/ct8.png"><p> Salem Cellular</p>';
-    div.innerHTML += '<img src="img/ct9.png"><p> US Cellular</p>';
+    div.innerHTML += '<i class="fa fa-signal marker-color-1"></i><p> New Cingular</p>';
+    div.innerHTML += '<i class="fa fa-signal marker-color-2"></i><p> Cello</p>';
+    div.innerHTML += '<i class="fa fa-signal marker-color-3"></i><p> RCC Minnesota</p>';
+    div.innerHTML += '<i class="fa fa-signal marker-color-4"></i><p> Verizon</p>';
+    div.innerHTML += '<i class="fa fa-signal marker-color-5"></i><p> US Cellular</p>';
+    div.innerHTML += '<i class="fa fa-signal marker-color-6"></i><p> Hood River Cellular</p>';
+    div.innerHTML += '<i class="fa fa-signal marker-color-7"></i><p> Medford Cellular</p>';
+    div.innerHTML += '<i class="fa fa-signal marker-color-8"></i><p> Oregon RSA</p>';
+    div.innerHTML += '<i class="fa fa-signal marker-color-9"></i><p> Salem Cellular</p>';
     // Return the Legend div containing the HTML content
     return div;
 };
