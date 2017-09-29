fe3374b560fffec6ab5752970b6986ec5f5fe5db
jakob <jakobzhao@gmail.com>
2017-09-29 10:56:31 -0700

    lab 04

--- lectures/lec05/js/select.js
+++ lectures/lec05/js/select.js
@@ -52,3 +52,16 @@ $(document).ready(function() {
             .animate({ marginLeft: 0}, 'fast');
     });
 });
+
+
+$("code").on({
+    mouseenter: function(){
+        $(this).css("background-color", "lightgray");
+    },
+    mouseleave: function(){
+        $(this).css("background-color", "lightblue");
+    },
+    click: function(){
+        $(this).css("background-color", "yellow");
+    }
+});
